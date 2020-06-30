34bab8557c81fb28ace9ec094b127de6f0cbec9b
jinwanmian <54888713+jinwanmian@users.noreply.github.com>
2020-06-30 20:18:42 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: '需要接口请加王红元老师微信：coderwhy003',
   timeout: 30000,
   responseType: 'json'
 });
+
+instance.interceptors.response.use(res =>{
+  return res.data;
+}, error => {
+  return Promise.reject(error);
+});
+
+export default instance;
